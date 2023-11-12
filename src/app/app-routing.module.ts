f9d108871e8b7f3eeedb3c549b927448467b7e11
pverma2905 <pverma2905@gmail.com>
2023-11-12 14:39:18 +0530

    app component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
 
 const routes: Routes = [
-  { path: 'form', loadChildren: () => import('./form/form.module').then(m => m.FormModule) }
+  { path: 'form', loadChildren: () => import('./form/form.module').then(m => m.FormModule) },
+  { path: '**', pathMatch: 'full',  component: AppComponent }, 
 ];
 
 @NgModule({
